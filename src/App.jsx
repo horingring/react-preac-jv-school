72ceb9894c0e3c0d7420d9674f1f48e391caf748
호링링\dldld <dldldksl@naver.com>
2024-03-08 12:50:11 +0900

    [add & test] styled-reset 적용 & 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
 import IntroPage from "@/pages/IntroPage";
+import styled from "styled-components";
+import { Reset } from "styled-reset";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  background-color: yellow;
+`;
 
 function App() {
   return (
-    <Wrapper>
-      App 컴포넌트
-      <IntroPage />
-      <Outlet />
-    </Wrapper>
+    <>
+      <Reset />
+      <Wrapper>
+        App 컴포넌트
+        <IntroPage />
+        <Outlet />
+      </Wrapper>
+    </>
   );
 }
 
--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,9 +1,10 @@
-import TestComponent from "@/components/IntroPage/TestComponent";
+// import TestComponent from "@/components/IntroPage/TestComponent";
+import TestComponent from "@components/IntroPage/TestComponent";
 
 function IntroPage() {
   return (
     <>
-      <h1>인트로 페이지 입니다</h1>
+      <h1 style={{ backgroundColor: "red" }}>인트로 페이지 입니다</h1>
       <TestComponent />
     </>
   );
